89b4bfdd46c2f8fcffbd17b7f5bea03d6a16003b
rozniak <roryfewell@hotmail.com>
2016-01-06 07:28:07 +0000

    Changing player and server to interfaces
    Added some more functions, made sense to change these to interfaces.
    
    I'll add some more functions and stuff later of course, and it might get
    cleaned up once I've tested it.

--- RozWorld-API/RozWorld-API/Entity/Player.cs
+++ RozWorld-API/RozWorld-API/Entity/Player.cs
@@ -14,66 +14,61 @@ using RozWorld_API.Level;
 
 namespace RozWorld_API.Entity
 {
-    public abstract class Player
+    public interface Player
     {
         /// <summary>
-        /// Gets or sets the username of this player.
+        /// Gets the username of this player.
         /// </summary>
-        public string Name
-        {
-            get { return _Name; }
-            set { if (_Name != null) _Name = value; }
-        }
-        private string _Name;
+        string Name { get; }
 
         /// <summary>
         /// Gets or sets the nickname of this player.
         /// </summary>
-        public abstract string Nickname { get; set; }
+        string Nickname { get; set; }
 
         /// <summary>
         /// Gets or sets the angle this player is facing.
         /// </summary>
-        public abstract double Angle { get; protected set; }
+        double Angle { get; set; }
 
         /// <summary>
         /// Gets the cardinal direction this player is facing.
         /// </summary>
-        public abstract CardinalDirection Direction { get; }
+        CardinalDirection Direction { get; }
 
         /// <summary>
         /// Gets or sets this player's game mode.
         /// </summary>
-        public abstract PlayerGameMode GameMode { get; set; }
+        PlayerGameMode GameMode { get; set; }
 
         /// <summary>
         /// Gets or sets this player's health.
         /// </summary>
-        public abstract uint Health { get; set; }
+        uint Health { get; set; }
 
         /// <summary>
         /// Gets or sets this player's maximum health.
         /// </summary>
-        public abstract uint MaxHealth { get; set; }
+        uint MaxHealth { get; set; }
 
         /// <summary>
         /// Gets or sets whether this player is dead or not.
         /// </summary>
-        public abstract bool IsDead { get; protected set; }
+        bool IsDead { get; }
 
         /// <summary>
         /// Gets or sets this player's movement speed.
         /// </summary>
-        public abstract ushort Speed { get; set; }
+        ushort Speed { get; set; }
 
         /// <summary>
-        /// Gets or sets the parent server of this player.
+        /// Gets this player's current speed.
         /// </summary>
-        protected Server ParentServer
-        {
-            get { return _ParentServer; }
-            set { if (_ParentServer == null) _ParentServer = value; }
-        }
-        private Server _ParentServer;
+        ushort CurrentSpeed { get; }
+
+        /// <summary>
+        /// Gets the parent server of this player.
+        /// </summary>
+        Server ParentServer { get; }
     }
 }
--- RozWorld-API/RozWorld-API/Server.cs
+++ RozWorld-API/RozWorld-API/Server.cs
@@ -20,72 +20,77 @@ using System.Collections.Generic;
 
 namespace RozWorld_API
 {
-    public abstract class Server
+    public interface Server
     {
         #region Server Properties
 
         /// <summary>
         /// Gets the version of this server.
         /// </summary>
-        public abstract ushort Version { get; }
+        ushort Version { get; }
 
         /// <summary>
         /// Gets the name of this server implementation.
         /// </summary>
-        public abstract string ImplementationName { get; }
+        string ImplementationName { get; }
 
         /// <summary>
         /// Gets whether this server is compatible with vanilla clients.
         /// </summary>
-        public abstract bool VanillaClientCompatible { get; }
+        bool VanillaClientCompatible { get; }
 
         /// <summary>
         /// Gets the list of client implementations allowed to join this server.
         /// </summary>
-        public IList<Tuple<string, ushort>> AllowedClientImplementations { get; protected set; }
+        IList<Tuple<string, ushort>> AllowedClientImplementations { get; }
         
         /// <summary>
         /// Gets or sets the max number of clients allowed on this server.
         /// </summary>
-        public abstract ushort MaxClients { get; protected set; }
+        ushort MaxClients { get; set; }
 
         /// <summary>
-        /// Gets or sets the list of whitelisted players on this server.
+        /// Gets the list of whitelisted players on this server.
         /// </summary>
-        public abstract IList<string> WhitelistedPlayers { get; protected set; }
+        IList<string> WhitelistedPlayers { get; }
 
         /// <summary>
         /// Gets or sets whether to whitelist this server.
         /// </summary>
-        public abstract bool WhitelistActive { get; set; }
+        bool WhitelistActive { get; set; }
 
         /// <summary>
-        /// Gets or sets the port that this server is running on.
+        /// Gets the port that this server is running on.
         /// </summary>
-        public abstract int Port { get; protected set; }
+        int Port { get; }
 
         /// <summary>
-        /// Gets or sets whether the server is active or not.
+        /// Gets whether the server is active or not.
         /// </summary>
-        public abstract bool IsActive { get; protected set; }
+        bool IsActive { get; }
 
         /// <summary>
-        /// Gets or sets whether the server is running on a LAN or purely local or not.
+        /// Gets whether the server is running on a LAN or purely local or not.
         /// </summary>
-        public abstract bool IsLocal { get; protected set; }
+        bool IsLocal { get; }
 
         /// <summary>
-        /// Gets or sets the logger on this server.
+        /// Gets the logger on this server.
         /// </summary>
-        public abstract Logger Log { get; protected set; }
+        Logger Log { get; }
+
+        /// <summary>
+        /// Gets or sets the difficulty of this server.
+        /// </summary>
+        byte Difficulty { get; set; }
 
         #endregion
 
         #region Game Events
 
-        public event PlayerChatEvent PlayerChatMessageSent;
-        public event PlayerGameModeEvent PlayerGameModeChanged;
-        public event LogEvent LogMessageSent;
+        event PlayerChatEvent PlayerChatMessageSent;
+        event PlayerGameModeEvent PlayerGameModeChanged;
+        event LogEvent LogMessageSent;
 
         #endregion
         
@@ -96,44 +101,59 @@ namespace RozWorld_API
         /// </summary>
         /// <param name="name">The player's username to locate them by.</param>
         /// <returns>The player if they exist on the server, null otherwise.</returns>
-        public abstract Player GetPlayer(string username);
+        Player GetPlayer(string username);
 
         /// <summary>
         /// Load or reloads the whitelist from the disk.
         /// </summary>
-        public abstract void LoadWhitelist();
+        void LoadWhitelist();
 
         /// <summary>
         /// Saves all players' data to the disk.
         /// </summary>
-        public abstract void SavePlayers();
+        void SavePlayers();
 
         /// <summary>
         /// Broadcasts a message to all players on the server.
         /// </summary>
         /// <param name="message">The message to broadcast.</param>
-        public abstract void BroadcastMessage(string message);
+        void BroadcastMessage(string message);
 
         /// <summary>
         /// Creates a new instance of the item with the exact specified name (case-sensitive).
         /// </summary>
         /// <param name="name">The exact case-sensitive name of the item.</param>
         /// <returns>A new instance of the item with the specified name.</returns>
-        public abstract Item CreateItem(string name);
+        Item CreateItem(string name);
 
         /// <summary>
         /// Gets the ID of an item by its exact specified name (case-sensitive).
         /// </summary>
         /// <param name="name">The exact case-sensitive name of the item.</param>
         /// <returns>The ID of the item if it exists, 0 otherwise.</returns>
-        public abstract ushort GetItemID(string name);
+        ushort GetItemID(string name);
 
         /// <summary>
         /// Registers a logger to this server instance if it does not have one already.
         /// </summary>
         /// <param name="logger">The logger to register.</param>
         /// <returns>Whether registering the logger was successful or not.</returns>
-        public abstract bool RegisterLogger(Logger logger);
+        bool RegisterLogger(Logger logger);
+
+        /// <summary>
+        /// Starts this server, as long as it isn't already started.
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// Reloads this server, as long as it is already started.
+        /// </summary>
+        void Reload();
+
+        /// <summary>
+        /// Stops this server, as long as it is already started.
+        /// </summary>
+        void Stop();
 
         #endregion
     }
