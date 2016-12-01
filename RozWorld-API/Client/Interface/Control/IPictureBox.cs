4d8327534ff712a470578bb6006070c46132c6b1
Rory Fewell <roryfewell@hotmail.com>
2016-12-01 23:57:31 +0000

    Add picturebox stuff

---
+++ RozWorld-API/Client/Interface/Control/IPictureBox.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * Oddmatics.RozWorld.API.Client.Interface.Control.IPictureBox -- RozWorld Client Interface Static Picture Box
+ *
+ * This source-code is part of the API for the RozWorld project by rozza of Oddmatics:
+ * <<http://www.oddmatics.uk>>
+ * <<http://roz.world>>
+ * <<http://github.com/rozniak/RozWorld-API>>
+ *
+ * Sharing, editing and general licence term information can be found inside of the "LICENCE.MD" file that should be located in the root of this project's directory structure.
+ */
+
+namespace Oddmatics.RozWorld.API.Client.Interface.Control
+{
+    /// <summary>
+    /// Represents a static picture box control on the interface.
+    /// </summary>
+    public interface IPictureBox : IControl
+    {
+        /// <summary>
+        /// The sizing mode to use on the image.
+        /// </summary>
+        PictureBoxSizeMode SizeMode { get; set; }
+
+        /// <summary>
+        /// The source image filepath to use.
+        /// </summary>
+        string Source { get; set; }
+    }
+}
---
+++ RozWorld-API/Client/Interface/Control/PictureBoxSizeMode.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * Oddmatics.RozWorld.API.Client.Interface.Control.PictureBoxSizeMode -- Picture Box and Image Control Image Sizing Mode
+ *
+ * This source-code is part of the API for the RozWorld project by rozza of Oddmatics:
+ * <<http://www.oddmatics.uk>>
+ * <<http://roz.world>>
+ * <<http://github.com/rozniak/RozWorld-API>>
+ *
+ * Sharing, editing and general licence term information can be found inside of the "LICENCE.MD" file that should be located in the root of this project's directory structure.
+ */
+
+namespace Oddmatics.RozWorld.API.Client.Interface.Control
+{
+    /// <summary>
+    /// Specifies constants defining the sizing mode of an image-related control.
+    /// </summary>
+    public enum PictureBoxSizeMode
+    {
+        /// <summary>
+        /// Represents that the picture should be tiled.
+        /// </summary>
+        Tiled,
+        /// <summary>
+        /// Represents that the picture should be stretched.
+        /// </summary>
+        Stretched
+    }
+}
